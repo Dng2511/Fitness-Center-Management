2eafe80d17a6ebda026f29532d679167d12d4250
Dng Pham <phamdong9854@gmail.com>
2025-05-07 22:43:02 +0700

    update room crud

---
+++ backend/src/main/java/com/example/backend/services/RoomService.java
@@ -0,0 +1,17 @@
+package com.example.backend.services;
+
+import com.example.backend.dtos.RoomDTO;
+
+import java.util.List;
+
+public interface RoomService {
+    List<RoomDTO> getRooms();
+
+    RoomDTO getRoomById(long id);
+
+    RoomDTO createRoom(RoomDTO roomDTO);
+
+    RoomDTO updateRoom(RoomDTO roomDTO, long id);
+
+    RoomDTO deleteRoom(long id);
+}
---
+++ backend/src/main/java/com/example/backend/services/implement/RoomServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.backend.services.implement;
+
+import com.example.backend.dtos.RoomDTO;
+import com.example.backend.models.Room;
+import com.example.backend.models.RoomStatus;
+import com.example.backend.models.RoomType;
+import com.example.backend.repositories.RoomRepository;
+import com.example.backend.services.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImpl implements RoomService {
+    private final RoomRepository roomRepository;
+
+
+    @Override
+    public List<RoomDTO> getRooms() {
+        return roomRepository.findAll().stream().map(RoomDTO::fromEntity).toList();
+    }
+
+    @Override
+    public RoomDTO getRoomById(long id) {
+        return RoomDTO.fromEntity(Objects.requireNonNull(roomRepository.findById(id).orElse(null)));
+    }
+
+    @Override
+    public RoomDTO createRoom(RoomDTO roomDTO) {
+        Room room = new Room();
+        room.setRoomName(roomDTO.getRoom_name());
+        room.setType(RoomType.valueOf(roomDTO.getType().toUpperCase()));
+        room.setStatus(RoomStatus.valueOf(roomDTO.getStatus().toUpperCase()));
+        roomRepository.save(room);
+        return null;
+    }
+
+    @Override
+    public RoomDTO updateRoom(RoomDTO roomDTO, long id) {
+        roomRepository.findById(id).ifPresent(room -> {
+            room.setRoomName(roomDTO.getRoom_name());
+            room.setType(RoomType.valueOf(roomDTO.getType().toUpperCase()));
+            room.setStatus(RoomStatus.valueOf(roomDTO.getStatus().toUpperCase()));
+            roomRepository.save(room);
+        });
+        return null;
+    }
+
+    @Override
+    public RoomDTO deleteRoom(long id) {
+        roomRepository.deleteById(id);
+        return null;
+    }
+}
