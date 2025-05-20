7d36d9c3d531ffbf1fee46a4b681084ed20bc4f2
Long Pham <longpd1911@gmail.com>
2025-05-20 20:46:39 +0700

    exception handling

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -1,19 +1,17 @@
 package com.example.backend.config;
 
-import com.example.backend.models.enums.UserRole;
+import com.example.backend.exception.CustomAuthenticationEntryPoint;
 import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
@@ -24,20 +22,22 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.crypto.spec.SecretKeySpec;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     final String[] PUBLIC_ENDPOINTS = {
             "/users", "/auth/login", "auth/introspect"
     };
 
     @Value("${jwt.secret}")
-    private String SIGNER_KEY;
+    String SIGNER_KEY;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -54,6 +54,11 @@ public class SecurityConfig {
         );
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.exceptionHandling(exceptionHandlingConfigurer ->
+                exceptionHandlingConfigurer
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+        );
 
         return httpSecurity.build();
     }
