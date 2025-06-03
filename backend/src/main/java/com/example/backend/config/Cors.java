283f91493c0fca1b56feadb28a9480ccac1d0e67
TADIZB <ductruong7d@gmail.com>
2025-06-03 21:48:29 +0700

    update cors & members

--- backend/src/main/java/com/example/backend/config/Cors.java
+++ backend/src/main/java/com/example/backend/config/Cors.java
@@ -10,7 +10,7 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Cho phép tất cả các đường dẫn
-                .allowedOrigins("http://localhost:3000") // Chỉ định nguồn được phép (thay bằng URL của React Admin)
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173") // Chỉ định nguồn được phép (thay bằng URL của React Admin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
                 .allowedHeaders("*")
                 .allowCredentials(true); // Cho phép tất cả các header
