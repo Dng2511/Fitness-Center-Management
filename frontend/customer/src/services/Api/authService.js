74b0da057a739efdf50b998536661fcca45cf6fb
Dng Pham <phamdong9854@gmail.com>
2025-06-07 14:36:29 +0700

    fix room[C

--- frontend/customer/src/services/Api/authService.js
+++
@@ -1,5 +0,0 @@
-import { Http } from "../Http";
-
-export const login = (data) => Http.post("/auth/login", data);
-
-export const register = (data) => Http.post("/users/register", data)
--- frontend/customer/src/services/Api/profile.js
+++
@@ -1,3 +0,0 @@
-import { Http } from "../Http";
-
-export const getProfile = () => Http.get("/users/my-info");
